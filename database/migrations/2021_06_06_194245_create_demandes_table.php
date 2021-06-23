589743d32158a83e0c01f0a4142f7c9763955251
Hicham <Hichamayat87@gmail.com>
2021-06-23 15:37:33 +0100

    add migration
    CreateDemandesTable  Migration

---
+++ database/migrations/2021_06_06_194245_create_demandes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDemandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demandes', function (Blueprint $table) {
+            $table->id();
+            $table->enum('etat_traitement',['','acceptée','validée','refusée','en_attente','abandonnée'])->default('');
+            $table->foreignId('utilisateur_id')->constrained('utilisateurs')->onUpdate('CASCADE')->onDelete('RESTRICT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demandes');
+    }
+}
