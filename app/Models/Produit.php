1c570d913fadf490171000561afcf35cc6f96750
Hicham <Hichamayat87@gmail.com>
2021-06-23 15:23:01 +0100

    Update modele
    add new model

---
+++ app/Models/Produit.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produit extends Model
+{
+    use HasFactory;
+        protected $fillable=['id',
+                            'designation',
+                            'qtestock',];
+    public function demande()
+    {
+        return $this->hasOne(R_Produit_DemandePrestation::class);
+    }
+    public function categorie()
+    {
+        return $this->belongsTo(categorie::class);
+    }
+
+}
