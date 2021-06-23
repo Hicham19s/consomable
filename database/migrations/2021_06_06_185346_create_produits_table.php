fd97275224660bd9c450219f69816743e6dd12e4
Hicham <Hichamayat87@gmail.com>
2021-06-23 15:35:34 +0100

    add migration
    CreateProduitsTable migration

---
+++ database/migrations/2021_06_06_185346_create_produits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->String('designation')->unique();
+            $table->UnsignedSmallInteger('qtestock');
+            $table->foreignId('categorie_id')->constrained('categories')->onUpdate('CASCADE')->onDelete('RESTRICT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
