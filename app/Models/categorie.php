aeab644b728bb2f8be0bf589920ded30a87a295c
Hicham <Hichamayat87@gmail.com>
2021-06-23 15:20:56 +0100

    Modificate fillable
    Add function produits

--- app/Models/categorie.php
+++ app/Models/categorie.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Produit;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,4 +11,9 @@ class categorie extends Model
     use HasFactory;
 
     protected $fillable=['designation',];
+
+    public function produits()
+    {
+        return $this->hasMany(Produit::class);
+    }
 }
