de3050cdd4fe9babe3280c0b930b326cfe1c9947
Hicham <Hichamayat87@gmail.com>
2021-06-28 20:38:38 +0100

    add ajax to view error

--- resources/views/utilisateur/ListeUtilisateurs.blade.php
+++ resources/views/utilisateur/ListeUtilisateurs.blade.php
@@ -8,7 +8,8 @@
 <div class="col-md-3 col-xl-2 bd-sidebar" style="background-color:#d6d8db;">
 <div class="container">
       <div id="html" class="col-md-8 offset-md-4 p-2" style="padding-top: 30px;margin-left: 0px;margin-top: 40px;margin-right: 0;">
-          <button data-toggle="modal" data-target="#formulaire" class="btn btn-primary" style="width: 160px;">Créer un utilisateur</button>
+          <button data-toggle="modal" data-target="#formulaire" 
+		  class="btn btn-primary" style="width: 160px;">Créer un utilisateur</button>
       </div>
     <div class="modal fade" id="formulaire">
       <div class="modal-dialog">
@@ -20,9 +21,9 @@
               </button>
           </div>
           <div class="modal-body row">
-                 <form action="/utilisateurs" method="post" >
+                 <form  method="post" >
                         @csrf
-                        @method('POST')
+                       
 	                        <div class="form-group row">
 	                            <label for="pseudo" class="col-md-4 col-form-label text-md-right">pseudo :</label>
 
@@ -136,15 +137,9 @@
 <div class="col-md-11 col-xl-8 pl-md-1 bd-content ">
 <div class="container ">    
                      
-                     <div class="col-md-6 justify-content-center">
-                     @if(Session::get('success'))
-                         <div class="alert alert-success alert-dismissible">
-                             <button class="close" data-dismiss="alert">X</button>
-                             {{Session::get('success')}}
-                         </div>
-                     </div>
+
                      
-                     @endif
+                     
              </div> 
 @livewire('search')
 </div>
@@ -157,6 +152,87 @@
  @livewireScripts
  </div>
 </div>
+<script>
+  function openModal() {
+            $('#formulaire').modal('show')
+        }
+		
+		function storeData() {
+           var CSRF_TOKEN =$('meta[name="csrf-token"]').attr('content');
+		   var pseudo =$('#pseudo').val();
+		   var nom =$('#nom').val();
+		   var prenom =$('#prenom').val();
+		   var nomservice =$('#nomservice').val();
+		   var password =$('#password').val();
+		   $.ajax({
+			   type 'Post',
+			   Url:'/utilisateurs',
+			   data :{
+				_token: CSRF_TOKEN,
+				pseudo:pseudo,
+				nom:nom,
+				prenom:prenom,
+				nomservice:nomservice,
+				password:password,
+			   }
+			   success: function (data) {
+              
+                },
+				error: function (data) {
+                    var errors = data.responseJSON;
+                    if($.isEmptyObject(errors) == false) {
+                        $.each(errors.errors,function (key, value) {
+                            var ErrorID = '#' + key +'Error';
+                            $(ErrorID).removeClass("d-none");
+                            $(ErrorID).text(value)
+                        })}}
+		   })
+        }
+		
+// </script>
+// <!-- <script type="text/javascript">
+//     $('body').on('click', '#submitForm', function(){
+//         var registerForm = $("#Register");
+//         var formData = registerForm.serialize();
+//         $( '#pseudo-error' ).html( "" );
+//         $( '#nom-error' ).html( "" );
+//         $( '#prenom-error' ).html( "" );
+//         $( '#password-error' ).html( "" );
+
+//         $.ajax({
+//             url:'/register',
+//             type:'POST',
+//             data:formData,
+//             success:function(data) {
+//                 console.log(data);
+//                 if(data.errors) {
+//                     if(data.errors.pseudo){
+//                         $( '#pseudo-error' ).html( data.errors.pseudo[0] );
+//                     }
+//                     if(data.errors.email){
+//                         $( '#nom-error' ).html( data.errors.nom[0] );
+//                     }
+// 					if(data.errors.email){
+//                         $( '#prenom-error' ).html( data.errors.prenom[0] );
+//                     }
+//                     if(data.errors.password){
+//                         $( '#password-error' ).html( data.errors.password[0] );
+//                     }
+                    
+//                 }
+//                 if(data.success) {
+//                     $('#success-msg').removeClass('hide');
+//                     setInterval(function(){ 
+//                         $('#formulaire').modal('hide');
+//                         $('#success-msg').addClass('hide');
+//                     }, 3000);
+//                 }
+//             },
+//         });
+//     });
+// </script> -->
 
 </body>
+
+
 @endsection
